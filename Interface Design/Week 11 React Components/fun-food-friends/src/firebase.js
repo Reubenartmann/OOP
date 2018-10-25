f8b066c4f289d418348017c0f463fb53a7966474
Reuben Artmann <101899555@student.swin.edu.au>
2018-10-25 12:11:33 +1100

    week 11 - react

---
+++ Interface Design/Week 11 React Components/fun-food-friends/src/firebase.js
@@ -0,0 +1,16 @@
+//<script src="https://www.gstatic.com/firebasejs/5.5.5/firebase.js"></script>
+
+import firebase from 'firebase'
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBUkDoaPEkQuSCgSZvSB7XK1Fwpfj_srlA",
+        authDomain: "week-11-react.firebaseapp.com",
+        databaseURL: "https://week-11-react.firebaseio.com",
+        projectId: "week-11-react",
+        storageBucket: "week-11-react.appspot.com",
+        messagingSenderId: "1055170509330"
+  };
+  firebase.initializeApp(config);
+
+export default firebase;
